035aad27c409bd35725d5a552050aa04386b0c30
Gavin Minami <gminami@evconnect.com>
2014-06-01 08:49:59 -0700

    map metro rail stations

--- Green Map App/js/loadStationJSON.js
+++ Green Map App/js/loadStationJSON.js
@@ -1,24 +1,67 @@
 /**
  * Created by marty on 5/31/14.
  */
-var blueStations={};
-var greenStations={};
-var expoStations= {};
-var goldStation = {};
-function loadStationJSON ()
+var blueStations=[];
+var greenStations=[];
+var expoStations= [];
+var goldStations = [];
+var rplineStations = [];
 
-{
-
-
-    blueStations=$.getJSON('JSON/blueline.json');
-    greenStations=$.getJSON('JSON/greenline.json');
-    expoStations=$.getJSON('JSON/expoline.json');
-    goldStation=$.getJSON('JSON/goldline.json');
-    console.log(blueStations);
-
+function loadStationJSON () {
+    $.getJSON('JSON/blueline.json', function(result){
+        result.data.forEach(function(st) {
+            console.log(st);
+            blueStations.push(parseStation(st));
+        });
+    });
+    $.getJSON('JSON/greenline.json', function(result){
+        result.data.forEach(function(st) {
+            greenStations.push(parseStation(st));
+        });
+    });
+    $.getJSON('JSON/expoline.json', function(result){
+        result.data.forEach(function(st) {
+            expoStations.push(parseStation(st));
+        });
+    });
+    $.getJSON('JSON/goldline.json', function(result){
+        result.data.forEach(function(st) {
+            goldStations.push(parseStation(st));
+        });
+    });
+    $.getJSON('JSON/rplines.json', function(result){
+        result.data.forEach(function(st) {
+            rplineStations.push(parseStation(st));
+        });
+    });
 }
 
+function getStationCoords(callback) {
+    loadStationJSON();
+    setTimeout(function() {
+        var all = [];
+        all.push.apply(all, blueStations);
+        all.push.apply(all, greenStations);
+        all.push.apply(all, expoStations);
+        all.push.apply(all, goldStations);
+        all.push.apply(all, rplineStations);
+        callback(all);
+    }, 100);
+}
 
+function parseStation(stationArr) {
+    return { latitude: stationArr[13], longitude:stationArr[12]};
+}
 
-
-
+function markStations(map) {
+    getStationCoords(function(stations) {
+        stations.forEach(function(station) {
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(station.latitude, station.longitude),
+                map: map,
+                title:"LA Metro Station",
+                icon: 'images/train-map-icon.png'
+            });
+        });
+    });
+}
