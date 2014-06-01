ad9b58c2f14e85dd4d54aab79b656e91284b13cc
Gavin Minami <gminami@evconnect.com>
2014-06-01 11:17:37 -0700

    add miles driven to fun facts

--- Green Map App/js/funfacts.js
+++ Green Map App/js/funfacts.js
@@ -1,19 +1,41 @@
 var ghgData = [];
+var milesDriven = [];
 
 function loadGhgData() {
     $.getJSON('JSON/ghg-emissions.json', function(result){
         result.data.forEach(function(st) {
-            console.log(st);
             ghgData.push({"year": st[8], "ghgTons": st[10]});
         });
     });
 }
 
+function loadMilesDrivenData() {
+    $.getJSON('JSON/miles-driven.json', function(result){
+        result.data.forEach(function(st) {
+            milesDriven.push({"year": st[8], "milesDriven": st[10]});
+        });
+    });
+}
+
 loadGhgData();
+loadMilesDrivenData();
 
 function getFunFact() {
-    var randomIndex = Math.floor(Math.random() * ghgData.length);
-    console.log(randomIndex);
-    var datum = ghgData[randomIndex];
-    return "Did you know LA operations produced " + datum.ghgTons + " tons of CO2 in " + datum.year;
+    var category = Math.floor(2 * Math.random());
+
+    if (category == 0) {
+
+        var randomIndex = Math.floor(Math.random() * ghgData.length);
+        console.log(randomIndex);
+        var datum = ghgData[randomIndex];
+        return "Did you know LA operations produced " + datum.ghgTons + " tons of CO2 in " + datum.year + "?";
+
+    } else {
+
+        var randomIndex = Math.floor(Math.random() * milesDriven.length);
+        console.log(randomIndex);
+        var datum = milesDriven[randomIndex];
+        return "Did you know " + datum.milesDriven + " miles were driven on LA roads in " + datum.year + "?";
+
+    }
 }
